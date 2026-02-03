734605a0a0182e899e79c66bc7a9a02bfc4a57b4
suprithashree <ssupritha336@gmail.com>
2026-02-03 13:34:04 +0530

    new chnages

--- class_object.py
+++ class_object.py
@@ -1,39 +1,66 @@
 class Instagram:
-    def __init__(self,title, description):  
+    
+    def __init__(self, title, description, creator_name, location):
         self.title = title
         self.description = description
+        self.creator_name = creator_name
+        self.location = location
         self.likes = 0
+        self.comments = []   
+    
+  
     def display_title(self):
-        print("The title of the reel is ",self.title)
+        print("The title of the reel is:", self.title)
+        
     def display_description(self):
-        print("The description of the reel is ",self.description)
+        print("The description of the reel is:", self.description)
+        
+    def display_creator(self):
+        print("Creator Name:", self.creator_name)
+        
+    def display_location(self):
+        print("Location:", self.location)
+        
     def display_likes(self):
-        print("The likes of the reel is ",self.likes)
+        print("The likes of the reel is:", self.likes)
+        
+    def display_comments(self):
+        print("Comments:")
+        if not self.comments:
+            print("No comments yet.")
+        else:
+            for comment in self.comments:
+                print("-", comment)
+    
+   
     def liked(self):
         self.likes += 1
+        
     def disliked(self):
         if self.likes > 0:
-            self.likes-=1
+            self.likes -= 1
+    
+   
+    def add_comment(self, comment):
+        self.comments.append(comment)
+    
+    
+    def delete_last_comment(self):
+        if self.comments:
+            removed = self.comments.pop()
+            print("Removed comment:", removed)
+        else:
+            print("No comments to delete.")
+reel1 = Instagram("Dancing", "Dancing with friends", 
+                  "Bharath P", "Mysore")
 
+reel1.add_comment("Awesome reel!")
+reel1.add_comment("Nice energy!")
+reel1.add_comment("Loved it!")
 
-reel1=Instagram("dancing","dancing with friends")
-# 0
-reel1.disliked() #0
-reel1.liked() #1
+reel1.display_comments()
 
+print("\nDeleting last comment...\n")
+reel1.delete_last_comment()
 
-
-
-reel2=Instagram("finance minister conference","finance minister conference with friends")
-reel1.liked() #2
-# 0
-reel2.liked() #1
-reel1.disliked() #1
-reel1.display_likes()
-reel2.display_likes()
-
-
-print(id(reel1))
-print(id(reel2))
-
-
+reel1.display_comments()
