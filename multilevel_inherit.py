246d92a7fbc5ec670ec1519e3c556c7813a6efbf
suprithashree <ssupritha336@gmail.com>
2026-02-04 21:46:43 +0530

    multilevel

---
+++ multilevel_inherit.py
@@ -0,0 +1,27 @@
+class Product:
+    def __init__(self, product_name, price):
+        self.product_name = product_name
+        self.price = price
+    def display_product(self):
+        print("Product Name:", self.product_name)
+        print("Price:", self.price)
+class ElectronicProduct(Product):
+    def __init__(self, product_name, price, brand, warranty):
+        super().__init__(product_name, price)
+        self.brand = brand
+        self.warranty = warranty
+    def display_electronic_product(self):
+        print("Brand:", self.brand)
+        print("Warranty:", self.warranty)
+class MobilePhone(ElectronicProduct):
+    def __init__(self, product_name, price, brand, warranty, ram, storage):
+        super().__init__(product_name, price, brand, warranty)
+        self.ram = ram
+        self.storage = storage
+    def display_mobile_details(self):
+        print("RAM:", self.ram)
+        print("Storage:", self.storage)
+phone1 = MobilePhone("iPhone 15", 80000, "Apple", "1 Year", "8GB", "256GB")
+phone1.display_product()
+phone1.display_electronic_product()
+phone1.display_mobile_details()
